4307eab741b5a82770d4c90199c419699bd81a82
Hidetoshi IMAI <hidepin@gmail.com>
2015-01-31 00:52:56 +0900

    ファイルを追加

---
+++ src/main/java/org/func/sample/hadoop/MaxTemperature.java
@@ -0,0 +1,22 @@
+package org.func.sample.hadoop;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.apache.hadoop.conf.Configuration;
+
+public class MaxTemperature {
+
+	public static void main(String[] args) {
+		Configuration conf = new Configuration();
+		conf.addResource("configuration.xml");
+		assertThat(conf.get("color"),is("yellow"));
+		assertThat(conf.getInt("size",0), is(10));
+		assertThat(conf.get("breadth"), "wide", is("wide"));
+		System.out.println(conf.get("color"));
+		System.out.println(conf.getInt("size",0));
+		System.out.println(conf.get("breadth","wide"));
+		System.out.println(conf.get("size-weight"));
+	}
+
+}
