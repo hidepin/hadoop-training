555aac76c8cafd2d56ee0efab0f1f920104e0127
Hidetoshi IMAI <hidepin@gmail.com>
2015-02-11 20:45:22 +0900

    Map関数を最適化

--- src/main/java/org/func/sample/hadoop/MaxTemperatureMapper.java
+++ src/main/java/org/func/sample/hadoop/MaxTemperatureMapper.java
@@ -11,14 +11,21 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class MaxTemperatureMapper extends
 		Mapper<LongWritable, Text, Text, IntWritable> {
 
+	private static final int MISSING = 9999;
+
 	public void map(LongWritable key, Text value, Context context)
 		throws IOException, InterruptedException {
 
 		String line = value.toString();
 		String year =line.substring(15, 19);
-		String temp = line.substring(87, 92);
-		if (!missing(temp)) {
-			int airTemperature = Integer.parseInt(temp);
+		int airTemperature;
+		if (line.charAt(87) == '+') {
+			airTemperature = Integer.parseInt(line.substring(88, 92));
+		} else {
+			airTemperature = Integer.parseInt(line.substring(87, 92));
+		}
+		String quality = line.substring(92,93);
+		if (airTemperature != MISSING && quality.matches("[01459]")) {
 			context.write(new Text(year), new IntWritable(airTemperature));
 		}
 	}
